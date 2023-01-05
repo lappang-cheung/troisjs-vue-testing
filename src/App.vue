4755355b155f5e13b360bb0f261671093fe89200
leocheung <lappangcheung@gmail.com>
2023-01-05 16:25:36 -0500

    Added the tailwind

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,18 @@ onMounted(() => {
 </script>
 
 <template>
-  <Renderer ref="renderer">
-    <Camera :position="{ z: 10 }" />
-      <Scene>
-        <PointLight :position="{ y: 50, z: 50 }" />
-        <Box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
-          <LambertMaterial />
-        </Box>
-      </Scene>
-  </Renderer>
+  <div class="p-0 m-0">
+    <Renderer width="400"
+              orbit-ctrl="true"
+              height="400"
+              ref="renderer">
+      <Camera :position="{ z: 10 }" />
+        <Scene background="#ffffff">
+          <PointLight :position="{ y: 50, z: 50 }" />
+          <Box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
+            <LambertMaterial />
+          </Box>
+        </Scene>
+    </Renderer>
+  </div>
 </template>
