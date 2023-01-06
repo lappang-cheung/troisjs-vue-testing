4a04eaaccbbe72ea11c28dea069154b6cda30828
Leo Cheung <lappangcheung@gmail.com>
2023-01-05 21:07:14 -0500

    Moving the position of the object

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@ const renderer = ref(null)
 const box = ref(null)
 
 const moveToLeft = () => {
-  let posZ = box.value.mesh.position.z
-  if (posZ > -5) {
-    return box.value.mesh.position.z -= 0.01
+  let posZ = box.value.mesh.position.x
+  if (posZ > -2.5) {
+    return box.value.mesh.position.x -= 0.01
   }
 }
 
@@ -20,8 +20,8 @@ const moveToBottom = () => {
 
 onMounted(() => {
   renderer?.value?.onBeforeRender(() => {
-    box.value.mesh.rotation.x += 0.005
-    moveToLeft()
+    // box.value.mesh.rotation.x += 0.005
+    // moveToLeft()
     moveToBottom()
   })
 })
@@ -29,16 +29,14 @@ onMounted(() => {
 
 <template>
   <div class="w-full h-screen">
-    <Renderer resize
-              :orbit-ctrl="true"
-              ref="renderer">
-      <Camera :position="{ x: 20, y:0, z:0 }" />
-        <Scene background="#ffffff">
-          <PointLight :position="{ y: 50, z: 50 }" />
-          <Box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
-            <MatcapMaterial name="392307_B3AE7D_6D5618_847C42" />
-          </Box>
-        </Scene>
+    <Renderer ref="renderer" resize>
+      <Camera :position="{ z: 10 }" />
+      <Scene>
+        <PointLight :position="{ y: 50, z: 50 }" />
+        <Box ref="box">
+          <LambertMaterial />
+        </Box>
+      </Scene>
     </Renderer>
   </div>
 </template>
