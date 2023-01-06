5674ef340d1c0f96af41a4c96c8de9b58e862ccf
Leo Cheung <lappangcheung@gmail.com>
2023-01-06 01:41:10 -0500

    Added the model into the scene

--- src/App.vue
+++ src/App.vue
@@ -18,24 +18,34 @@ const moveToBottom = () => {
   }
 }
 
+const onReady = () => {
+
+}
+
+const onProgress = () => {
+
+}
+
+const onError = () => {
+
+}
+
 onMounted(() => {
-  renderer?.value?.onBeforeRender(() => {
-    // box.value.mesh.rotation.x += 0.005
-    // moveToLeft()
-    moveToBottom()
-  })
+  // renderer?.value?.onBeforeRender(() => {
+  //   // box.value.mesh.rotation.x += 0.005
+  //   // moveToLeft()
+  //   // moveToBottom()
+  // })
 })
 </script>
 
 <template>
   <div class="w-full h-screen">
-    <Renderer ref="renderer" resize>
+    <Renderer ref="renderer" :orbit-ctrl="true" resize antialias>
       <Camera :position="{ z: 10 }" />
-      <Scene>
-        <PointLight :position="{ y: 50, z: 50 }" />
-        <Box ref="box">
-          <LambertMaterial />
-        </Box>
+      <Scene background="#ffffff">
+        <AmbientLight></AmbientLight>
+        <GltfModel src="tricks_for_treats/scene.gltf" />
       </Scene>
     </Renderer>
   </div>
